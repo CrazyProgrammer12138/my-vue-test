0194fd4a4f4a8267a60bcf624ed68865f7be36e3
CrazyProgrammer12138 <1525809031@qq.com>
2019-04-09 00:13:12 +0800

    add promise

---
+++ 04node/node-day2/node-code-block/node-day2-code/4.fs.js
@@ -0,0 +1,38 @@
+// fileSystem 文件系统  文件的读写
+let fs = require('fs');
+// 既有同步又有异步方法，异步有callback
+
+
+// 同步的读取
+// 1.读取文件，文件必须存在, 不能通过 / 读取内容 / 表示根目录
+// 2.读取的默认类型就是buffer
+let result = fs.readFileSync('4.fs.js', 'utf8');
+console.log(result)
+
+//异步的方案，会导致回调地狱，不方便维护
+fs.readFile('./1.txt', 'utf8', function(err,data){
+    if (err) return console.log(err)
+    fs.readFile(data, 'utf8', function (err) {
+        if (err) return console.log(err)
+        console.log(data)
+    })
+})
+let util = require('util')
+let read = util.promisify(fs.readFile)
+read('./1.txt', 'utf8').then(function (data) {
+    // 如果第一个promise中返回了一个promise实例，会把当前执行的结果传到下一个then中
+    console.log(data)
+}).then((data)=>{
+    console.log(data)
+},0).catch((data)=>{
+    // 错误处理，如果写了错误callback走自己的，没写统一走catch
+    console.log(data)
+})
+// await 后面只能跟随promsie
+async function result() {
+    let context1 = await read('./1.txt', utf8)
+    let context2 = await read(data, utf8)
+    let str = context2 + 'sjs'
+    console.log(str)
+}
+result();
