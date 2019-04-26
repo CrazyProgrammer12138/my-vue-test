b61081de546ec4bcab8f6c6f000a6c80dd5f7d9d
lianshengjinfu <qinmingyue@bjthls.cn>
2019-04-26 18:29:00 +0800

    promise01

---
+++ promise/promise-01/promise-01-code/1.highorder.js
@@ -0,0 +1,39 @@
+/**
+ * 1.再js里，函数是一等公民，可以作为函数的返回值，也可以作为函数的参数
+ */
+// 判断一个 参数是否是字符串
+function isString(param) {
+  return Object.prototype.toString.call(param) == '[object String]'
+}
+
+// 判断一个 参数是否是数组
+function isArray(param) {
+  return Object.prototype.toString.call(param) == '[object Array]'
+}
+
+// 函数可以作为返回值
+function isType(type) {
+  return function(param) {
+    return Object.prototype.toString.call(param) == `[object ${type}]`
+  }
+}
+
+console.log(isString({}))
+console.log(isArray([]))
+
+//lodash after  指定一个函数被调用多少次才会真正执行
+// 函数可以作为参数传到另外一个函数里面
+function eat() {
+  console.log('eat')
+}
+function after(times, fn) {
+  let count = 0
+  if (++count == 3) {
+    fn()
+  }
+}
+
+let newEat = new after(3, eat)
+newEat()
+newEat()
+newEat()
---
+++ promise/promise-01/promise-01-code/2.callback.js
@@ -0,0 +1,89 @@
+// /**
+//  * 比如现在要读取一个文件，异步读取
+//  */
+let fs = require('fs')
+
+// /**
+//  * 回调得特点是error  first
+//  * 调用回调函数得时候第一个参数永远是错误对象
+//  */
+// fs.readFile('./1.txt', 'utf8', function(err, data) {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     console.log(data)
+//   }
+// })
+// /**
+//  * 回调函数得问题
+//  * 1、无法捕获错误 try  catch return
+//  * 2、不能return
+//  */
+// function read(filename) {
+//   fs.readFile(filename, 'utf8', function(err, data) {
+//     if (err) {
+//       console.log(err)
+//     } else {
+//       console.log(data)
+//     }
+//   })
+// }
+
+// try {
+//   read('./1.txt')
+// } catch (e) {
+//   console.log('err' + e)
+// }
+/**
+ * 当你访问服务器得时候，比如要请求一个HTML页面，比如是用户列表，
+ * 服务器一方面会去读取模板文件，可能是ejs pug jade handlebar,
+ * 另外一方面还要读取数据（可能会放在文件里，也可能会放在数据里），它们都很慢，所以都是异步的
+ * 这种恶魔金字塔有一下问题
+ * 1、非常难看
+ * 2、非常难以维护
+ * 3、效率比较低，因为他们是串行得
+ * 4、如何解决这个回调嵌套问题
+ *    1、通过事件发布订阅来实现
+ *    2、
+ */
+// 这是node核心模块中得一个类，通过它可以创建事件发射器得实例，里面有两个核心方法，
+// 一个in emit，on表示注册监听，emit表示发射事件
+// let EventEmitter = require('events')
+// let eve = new EventEmitter()
+// let html = {}
+// eve.on('ready', function(key, value) {
+//   html[key] = value
+//   if (Object.keys(html).length == 2) {
+//     console.log(html)
+//   }
+// })
+
+// fs.readFile('./template.txt', 'utf8', function(err, data) {
+//   eve.emit('ready', 'template', data)
+// })
+// fs.readFile('./data.txt', 'utf8', function(err, data) {
+//   eve.emit('ready', 'data', data)
+// })
+// 通过一个哨兵函数来处理
+
+function render(len, cb) {
+  let html = {}
+  return function(key, value) {
+    html[key] = value
+    if (Object.keys(html).length == len) {
+      cb(html)
+    }
+  }
+}
+let done = render(2, function(html) {
+  console.log(html)
+})
+fs.readFile('./template.txt', 'utf8', function(err, data) {
+  console.log(data)
+
+  done('template', data)
+})
+fs.readFile('./data.txt', 'utf8', function(err, data) {
+  console.log(data)
+  done('data', data)
+})
